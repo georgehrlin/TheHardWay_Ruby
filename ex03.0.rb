f0586620d9a850860287706424e32795a567d991
George Lin <linhairan@gmail.com>
2023-03-06 18:48:09 +0000

    Finish exercise 03. Working on ex04.rb

---
+++ ex03.0.rb
@@ -0,0 +1,43 @@
+# Print a string
+puts "I will now count my chickens:"
+
+# Print a string with a math expression interpolated inside
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# Print a string with a math expression interpolated inside
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Print a string
+puts "Now I will count the eggs:"
+
+# Print the result of a math calculation
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Print a string
+puts "Is it true that 3 + 2 < 5 - 7"
+
+# Print the result of a math calcualtion
+puts 3.0 + 2 < 5 - 7
+
+# Print a string with a math expression interpolated inside
+puts "What is 3 + 2? #{3.0 + 2}"
+# Print a string with a math expression interpolated inside
+puts "What is 5 - 7? #{5.0 - 7}"
+
+# Print a string
+puts "Oh, that's why it's false."
+
+# Print a string
+puts "How about some more."
+
+# Print a string with a math expression interpolated inside
+puts "Is it greater? #{5.0 > -2}"
+# Print a string with a math expression interpolated inside
+puts "Is it greater or equal? #{5.0 > -2}"
+# Print a string with a math expression interpolated inside
+puts "Is it less or equal? #{5.0 <= -2}"
+
+# Test
+puts "I want to write a math formula in a pair of braces."
+puts "\{ax**2 + bx + c = 0\}"
+puts "4 % 2 is #{4.0 % 2}."
+puts "1 / 4 is #{1.0 / 4}."
---
+++ ex03.1.rb
@@ -0,0 +1,10 @@
+planned_stu_h_p_day = 3.0
+stu_day_p_week = 6
+stu_h_p_week = planned_stu_h_p_day * stu_day_p_week
+avg_stu_h_p_day = stu_h_p_week / 7
+
+puts avg_stu_h_p_day
+
+stu_h_p_year = avg_stu_h_p_day * 365
+
+puts stu_h_p_year
