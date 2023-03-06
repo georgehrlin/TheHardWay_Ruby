5a319bda4095fb5d61b873826447b6cfa78ea520
George Lin <linhairan@gmail.com>
2023-03-06 22:54:23 +0000

    Finish ex07.rb. Create mistakes.txt

---
+++ ex07.rb
@@ -0,0 +1,29 @@
+# Print a string
+puts "Mary had a little lamb."
+# Print a string that has a string interpolated in it
+puts "Its fleece was white as #{'snow'}."
+# Print a string
+puts "And everywhere that Mary went."
+# Print a string that is the result of "." being copied ten times
+puts "." * 10 # what'd that do? Oh I know, it will make a string with 10 "."
+
+# Create a variable and assign a string as its value
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# Print the concatenation of the variables end1 to end6 without a newline
+# character at its end
+print end1 + end2 + end3 + end4 + end5 + end6
+# Print the concatenation of the variables end7 to end12
+puts end7 + end8 + end9 + end10 + end11 + end12
