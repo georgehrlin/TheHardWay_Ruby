f27ff742c0c3a405e4fb0b88688364387671c9e7
George Lin <linhairan@gmail.com>
2023-03-08 16:10:43 +0000

    Finish Exercise 12

---
+++ ex12.0.rb
@@ -0,0 +1,20 @@
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+# Study Drills
+# 1
+bigger_float = bigger.to_f
+puts "I made the bigger number into a floating point number: #{bigger_float}!"
+
+smaller_float = smaller.to_f
+puts "I also did the same to the smaller number: #{smaller_float}!"
