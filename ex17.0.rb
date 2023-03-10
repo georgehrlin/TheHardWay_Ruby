45d400350c6e1126f98bf97db5ea8a6c5cd41ff6
George Lin <linhairan@gmail.com>
2023-03-10 18:26:17 +0000

    Finish Exercise 17

---
+++ ex17.0.rb
@@ -0,0 +1,29 @@
+from_file, to_file = ARGV
+
+puts "Copying from #{from_file} to #{to_file}"
+
+# we could do these two on one line, how?
+in_file = open(from_file)
+indata = in_file.read
+
+puts "The input file is #{indata.length} bytes long"
+
+puts "Does the output file exist? #{File.exist?(to_file)}"
+puts "Ready, hit RETURN to continue, CTRL-C to abort."
+$stdin.gets
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+puts "Alright, all done."
+
+out_file.close
+in_file.close
+
+=begin
+Study Drills
+4 Closing files manually or by using blocks is necessary because the system has 
+  a limited number of file descriptors. (File descriptors are entries the OS 
+  creates to "represent that file and store the information about that opened
+  file".) Closing files also leads to conserving memory.
+=end
---
+++ ex17.1.rb
@@ -0,0 +1,2 @@
+# Study Drills 2
+open(ARGV[1], 'w').write(open(ARGV[0]).read)
